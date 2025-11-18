4ba45398a8efa4e32a3973cf4634590f3f712484
rraxnt-maker <rraxnt@gmail.com>
2025-11-18 20:31:25 +1000

    pygame

--- GuessGame/GuessN.py
+++ GuessGame/GuessN.py
@@ -1,8 +1,21 @@
 import random
 
 def guess_the_number():
+
     number = random.randint(1, 100)
     trys = 0
-    print(trys, number)
+
+    while True:
+        inp = int(input("Try to guess number: "))
+        trys += 1
+
+        if inp < number:
+            print("The number is bigger")
+        elif inp > number:
+            print ("The number is lesser")
+        else:
+            print(f"wow actually you made it in {trys} tries")
+            break
+
 
 guess_the_number()
